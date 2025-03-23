f034bbca0ebf7228789db983d5a27de87c0e13cf
Lavande <admin@5dimn.com>
2025-03-23 21:43:21 +0800

    fixed pin function (uuid)

--- lib/utils/helpers.ts
+++ lib/utils/helpers.ts
@@ -1,10 +1,11 @@
 import { Cocktail } from '@/types';
+import { v4 as uuidv4 } from 'uuid';
 
 /**
  * 生成唯一ID
  */
 export function generateId(): string {
-  return Math.random().toString(36).substring(2, 11);
+  return uuidv4();
 }
 
 /**
